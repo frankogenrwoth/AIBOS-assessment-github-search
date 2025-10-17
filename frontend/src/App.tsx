45c225e953e4333bb341d84cd7824cac2dde1906
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-10-17 23:01:05 +0300

    add final changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
 import type { Repository } from './types';
 import RepositoriesList from './components/RepositoriesList';
@@ -7,14 +7,42 @@ function App() {
   const [count, setCount] = useState(0);
 
   const [paginationSize, setPaginationSize] = useState(20);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newSearch, setNewSearch] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const [repositories, setRepositories] = useState<Repository[]>([])
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [filter, setFilter] = useState('all');
+  const [sort, setSort] = useState('best');
+  const [orderedRepositories, setOrderedRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:8000/api/v1/repositories', {credentials: 'include'})
       .then((response) => response.json())
-      .then((data) => { setRepositories(data); setCount(data.length) })
+      .then((data) => { 
+        setRepositories(data);
+        setCount(data.length);
+
+        if (filter == 'all') {
+          setOrderedRepositories(data);
+        } else {
+          setOrderedRepositories(data.filter((repo: Repository) => repo.language?.toLowerCase() === filter));
+        }
+        if (sort == 'best') {
+          setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.score - a.score));
+        } else if (sort == 'stars') {
+          setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.stargazers_count - a.stargazers_count));
+        } else if (sort == 'watchers') {
+          setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.watchers_count - a.watchers_count));
+        } else if (sort == 'size') {
+          setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.size - a.size));
+        } else if (sort == 'issues') {
+          setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.open_issues - a.open_issues));
+        } else if (sort == 'forks') {
+          setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.forks - a.forks));
+        }
+        
+      })
       .catch((error) => console.error('Error fetching repositories:', error))
   }, []);
 
@@ -22,13 +50,49 @@ function App() {
     setCurrentPage(1);
   }, [paginationSize]);
 
+  useEffect(() => {
+    if (filter == 'all') {
+      setOrderedRepositories(repositories);
+    } else {
+      setOrderedRepositories(repositories.filter((repo: Repository) => repo.language?.toLowerCase() === filter));
+    }
+    setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b?.score - a.score));
+  }, [filter, repositories]);
+
+  useEffect(() => {
+    if (sort == 'best') {
+      setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.score - a.score));
+    } else if (sort == 'stars') {
+      setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.stargazers_count - a.stargazers_count));
+    } else if (sort == 'watchers') {
+      setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.watchers_count - a.watchers_count));
+    } else if (sort == 'size') {
+      setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.size - a.size));
+    } else if (sort == 'issues') {
+      setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.open_issues - a.open_issues));
+    } else if (sort == 'forks') {
+      setOrderedRepositories((prev) => prev.sort((a: Repository, b: Repository) => b.forks - a.forks));
+    }
+  }, [sort]);
+
+  useEffect(() => {
+    setCount(orderedRepositories.length);
+  }, [orderedRepositories]);
+
+  function searchKeyword() {
+    fetch(`http://localhost:8000/api/v1/search?q=${searchTerm}`, { credentials: 'include' })
+      .then((response) => response.json())
+      .then((data) => { setRepositories(data); setCount(data.length); setNewSearch(true); })
+      .catch((error) => console.error('Error fetching repositories:', error))
+  }
+
   return (
       <div className="wrapper">
         <div className="header">
           <div className="search">
             <div className="search-box">
-              <input type="text" placeholder="Search repositories..." />
-              <button>Search</button>
+              <input type="text" placeholder="Search repositories..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+              <button onClick={() => {searchKeyword()}}>Search</button>
             </div>
             
           </div>
@@ -36,9 +100,13 @@ function App() {
           <div className="action">
             <div className="sort">
               <label htmlFor="sort-by">Sort by:</label>
-              <select className="form-control select-wrapper" id="sort-by" defaultValue="best" aria-label="Sort repositories">
+              <select className="form-control select-wrapper" id="sort-by" defaultValue="best" aria-label="Sort repositories" onChange={() => {}}>
                 <option value="best">Best match</option>
                 <option value="stars">Most stars</option>
+                <option value="watchers">most watchers</option>
+                <option value="size">size</option>
+                <option value="issues">most issues</option>
+                <option value="forks">Most forks</option>
               </select>
             </div>
             <div className="pagination-size">
@@ -63,11 +131,38 @@ function App() {
         </div>
 
         <div className="search-result">
-        <div className="result-count">{count} results found in <span className="search-time">"your search term"</span></div>            
+          <div className="result-count">{count} results {(searchTerm && newSearch) && (<>found in <span className="search-time">"{searchTerm}"</span></> )}</div>
+          
+          <div className="filters">
+              <div className="filter-chip">
+                <input className='filter-chip-input' id="all" name="all-filter" type="checkbox" value="all" checked={filter === 'all'} onChange={() => setFilter('all')} /> 
+                <label className='filter-chip-label' htmlFor="all">All</label>
+              </div>
+              <div className="filter-chip">
+            <input className='filter-chip-input' id="python" name="python-filter" type="checkbox" value="python" checked={filter === 'python'} onChange={() => setFilter('python')} /> 
+                <label className='filter-chip-label' htmlFor="python">Python</label>
+              </div>
+              <div className="filter-chip">
+            <input className='filter-chip-input' id="js" name="js-filter" type="checkbox" value="javascript" checked={filter === 'javascript'} onChange={() => setFilter('javascript')} /> 
+                <label className='filter-chip-label' htmlFor="js">JS</label>
+
+              </div>
+              <div className="filter-chip">
+            <input className='filter-chip-input' id="typescript" name="typescript-filter" type="checkbox" value="typescript" checked={filter === 'typescript'} onChange={() => setFilter('typescript')} /> 
+                <label className='filter-chip-label' htmlFor="typescript">Typescript</label>
+                
+              </div>
+              <div className="filter-chip">
+            <input className='filter-chip-input' id="c" name="c-filter" type="checkbox" value="c" checked={filter === 'c'} onChange={() => setFilter('c')} /> 
+                <label className='filter-chip-label' htmlFor="c">C</label>
+                
+              </div>
+
+          </div>
         </div>
 
         <div className="container">
-          <RepositoriesList repositories={repositories.slice((currentPage - 1) * paginationSize, currentPage * paginationSize)} />
+          <RepositoriesList repositories={orderedRepositories.slice((currentPage - 1) * paginationSize, currentPage * paginationSize)} />
         </div>
 
         <div className="pagination">
--- frontend/src/types/repository.ts
+++ frontend/src/types/repository.ts
@@ -107,5 +107,5 @@ export interface Repository {
   open_issues: number;
   watchers: number;
   default_branch: string;
-  score?: number;
+  score: number;
 }
