447ccc6dc865c63410addb71cccc619305263801
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-10-17 16:25:07 +0300

    add the api calls and add the repository data type in the frontend also fix the cors issues

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%vtxavwe76%d0ayyo7do#3_8zx(o6xj3k@7g@4^#tnf-a1tsrm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,13 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
     "rest_framework",
+    "corsheaders",
+    "api",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -129,3 +131,10 @@ MEDIA_ROOT = "mediafiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import type { Repository } from './types';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/v1/repositories', {credentials: 'include'})
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+      .catch((error) => console.error('Error fetching repositories:', error))
+  }, []);
 
   return (
     <>
@@ -17,6 +27,25 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      {repositories.length > 0 ? (
+        <div>
+          <h2>Repositories:</h2>
+          <ul>
+            {repositories.map((repo) => (
+              <li key={repo?.id}>
+                <h3>{repo?.name}</h3>
+                <p>{repo?.description}</p>
+                <a href={repo?.url} target="_blank" rel="noopener noreferrer">
+                  Visit Repository
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading repositories...</p>
+      )}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
