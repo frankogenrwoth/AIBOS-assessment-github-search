05a3fb0bd8358a5c791929907ef0f635c774a682
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-10-17 08:05:05 +0300

    refactor the call runserver which was a blocking command

--- setup.bat
+++ setup.bat
@@ -3,14 +3,10 @@
 @REM Current directory
 SET "CURRENT_DIR=%~dp0"
 
-@REM Change to backend directory
-cd /d "%CURRENT_DIR%backend"
-
-@REM Activate virtual environment
-call "%CD%\.venv\Scripts\activate.bat"
-
-@REM Start backend server
-call python manage.py runserver
+@REM Start backend server in a new terminal
+start "Backend" ^
+    /D "%CURRENT_DIR%backend" ^
+    "%ComSpec%" /k "call .venv\Scripts\activate.bat && python manage.py runserver"
 
 @REM Start frontend in a new terminal
 start "Frontend" ^
