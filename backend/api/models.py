55fc2b83013b76ec10de78cebb94e40df2360bbd
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-10-17 08:20:16 +0300

    add the api models for saving search data in the local database

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __getattr__(self, attr):
+        if attr in self.data:
+            return self.data[attr]
+        raise AttributeError(
+            f"'{self.__class__.__name__}' object has no attribute '{attr}'"
+        )
+
+    def __str__(self):
+        return self.name or ""
+
+    def save(self, *args, **kwargs):
+        """
+        Override save to ensure 'name' is always set from 'full_name' before saving.
+        """
+        self.name = getattr(self, 'full_name', self.name)
+        return super().save(*args, **kwargs)
